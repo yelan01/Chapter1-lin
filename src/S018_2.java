368d16679095292ae95a0bbb9f2e54dfce0dc07c
yelan01 <161634843+yelan01@users.noreply.github.com>
2025-10-17 21:50:46 +0800

    Add files via upload

---
+++ src/S018_2.java
@@ -0,0 +1,8 @@
+public class S018_2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("八進位數101的字元是\101");
+        System.out.println("十六進位數0061的字元是\u0061");
+    }
+}
